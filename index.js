60d566db469f786eb97074cc8328cce2519d4c53
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-09-13 06:22:30 +0100

    Wrote a script to alert icecream preference.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-let firstName = Elozino;
-let middleName = Lopez;
-let lastName = ASAIJE;
-console.log('lastName, firstName, middleName');
+// let firstName = 'Elozino';
+// let middleName = 'Lopez';
+// let lastName = 'ASAIJE';
+// console.log(lastName, firstName, middleName);
+
+// console.log(Hey);
+
+// alert('Hey!');
+
+let iceCream = 'chocolate';
+
+if (iceCream === 'chocolate') {
+    alert ('Yes! I love Chocolate!!');
+} else {
+    alert ('Awnn... but I love chocolate...');
+}
